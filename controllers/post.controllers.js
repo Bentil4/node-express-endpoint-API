6bc5ce39d40de92124d1983aed8b18f311cd0c10
Broderick Bentil <bentilfrederick4@gmail.com>
2024-06-07 23:37:29 +0000

    adding post controllers

---
+++ controllers/post.controllers.js
@@ -0,0 +1,11 @@
+const Product = require('../models/product.model.js');
+
+// POST CONTROLLER
+exports.addProducts = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
